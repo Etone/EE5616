9753d5b0e45b4cc47649395cf568d8b3dcb6de56
Michael Watzko <michael@watzko.de>
2018-09-21 11:18:54 +0200

    Add PointTest with kinda mock Point class

---
+++ ee5616_2018/Point.java
@@ -0,0 +1,45 @@
+package ee5616_2018;
+
+public class Point {
+
+	public Point() {
+
+	}
+	
+	public Point(double x, double y) {
+		
+	}
+	
+	public double getX() {
+		return Double.NaN;
+	}
+	
+	public void setX(double x) {
+
+	}
+	
+	public double getY() {
+		return Double.NaN;
+	}
+	
+	public void setY(double y) {
+
+	}
+	
+	public double norm() {
+		return Double.NaN;
+	}
+	
+	public void rotate(double theta) throws AngleOutOfRangeException {
+		
+	}
+	
+	public void displace(Point p) {
+		
+	}
+	
+	public class AngleOutOfRangeException extends Exception {
+		
+	}
+
+}
