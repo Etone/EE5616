0897f41f00ba2062f762a346320c217763d47a72
Michael Watzko <michael@watzko.de>
2018-09-21 16:14:31 +0200

    Add neccessary log for evaluation

--- SLF/examplefReadBack.java
+++ SLF/examplefReadBack.java
@@ -10,7 +10,12 @@
 
 package SLF;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import ee5616_2018.Line;
+import ee5616_2018.Point;
 import uk.ac.brunel.ee.lineRead;
 import uk.ac.brunel.ee.UnreadException;
 import uk.ac.brunel.ee.RereadException;
@@ -29,9 +34,13 @@ public class examplefReadBack {
 // Open the file and initialise
 		lineRead reader = new lineRead("data.dat");
 
+		List<Line> lines = new ArrayList<>();
+		
 // Loop over all the lines in the data set		
 		while (reader.nextLine()) {
 			boolean np=true;
+			Line line = new Line();
+			lines.add(line);
 // Loop over all the points associated with the current line
 			while (np) {
 			    try {
@@ -45,6 +54,10 @@ public class examplefReadBack {
 				    try {
 					   x = reader.getX();
 					   y = reader.getY();
+					   
+					   Point p = new Point(x, y);
+					   line.add(p);
+					   
 //
 //      >> do the fitting here
 //
@@ -61,5 +74,29 @@ public class examplefReadBack {
 	    long fin = end.getTime();
 	    System.out.println("run time is "+ (fin-begin)+" milliseconds");
 	    
+	    
+	    System.out.println("Lines: "+lines.size());
+	    int valid = 0;
+	    int validLength = 0;
+	    int invalidLength = 0;
+	    for (Line line : lines) {
+	    	if (line.isValid()) {
+	    		try {
+		    		System.out.println("  - line length="+line.length()+" slope="+line.slope()+", intersect="+line.intercept());
+		    		valid += 1;
+		    		validLength += line.length();
+	    		} catch (Throwable t) {
+	    			System.err.println("Unexpected invalid line");
+	    			t.printStackTrace();
+	    		}
+	    	} else {
+	    		invalidLength += line.length();
+	    	}
+	    }
+	    
+	    System.out.println("Valid lines: "+valid);
+	    System.out.println("Invalid lines: "+(lines.size() - valid));
+	    System.out.println("Average valid line length: "+((double)validLength / (double)valid));
+	    System.out.println("Average invalid line length: "+((double)invalidLength / (double)(lines.size() - valid)));
 	}
 }
